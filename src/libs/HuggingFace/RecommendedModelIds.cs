1270fe9bac80568f65ebbf6ee2a46f89d13d7c7d
Konstantin S <havendv@gmail.com>
2023-08-31 13:44:54 +0400

    fix: Added Llama2 to RecommendedModelIds.

--- src/libs/HuggingFace/RecommendedModelIds.cs
+++ src/libs/HuggingFace/RecommendedModelIds.cs
@@ -7,11 +7,46 @@ namespace HuggingFace;
 /// Recommended models for HuggingFace.
 /// </summary>
 public static class RecommendedModelIds
-{ 
+{
     /// <summary>
     /// https://huggingface.co/gpt2 <br/>
     /// Pretrained model on English language using a causal language modeling (CLM) objective.
     /// It was introduced in this paper and first released at this page. <br/>
     /// </summary>
     public const string Gpt2 = "gpt2";
+
+    /// <summary>
+    /// https://huggingface.co/meta-llama/ <br/>
+    /// Llama 2 is a collection of pretrained and fine-tuned generative text models ranging in scale from 7 billion to 70 billion parameters.
+    /// This is the repository for the 70B fine-tuned model, optimized for dialogue use cases and converted for the Hugging Face Transformers format.
+    /// Links to other models can be found in the index at the bottom. <br/>
+    /// </summary>
+    public static class Llama2
+    {
+        /// <inheritdoc cref="Llama2"/>
+        public static class Base
+        {
+            /// <inheritdoc cref="Llama2"/>
+            public const string _7B = "meta-llama/Llama-2-7b-hf";
+        
+            /// <inheritdoc cref="Llama2"/>
+            public const string _13B = "meta-llama/Llama-2-13b-hf";
+        
+            /// <inheritdoc cref="Llama2"/>
+            public const string _70B = "meta-llama/Llama-2-70b-hf";
+        }
+
+        /// <inheritdoc cref="Llama2"/>
+        public static class Chat
+        {
+            /// <inheritdoc cref="Llama2"/>
+            public const string _7B = "meta-llama/Llama-2-7b-chat-hf";
+        
+            /// <inheritdoc cref="Llama2"/>
+            public const string _13B = "meta-llama/Llama-2-13b-chat-hf";
+        
+            /// <inheritdoc cref="Llama2"/>
+            public const string _70B = "meta-llama/Llama-2-70b-chat-hf";
+        }
+    }
 }
--- src/tests/HuggingFace.IntegrationTests/Tests.cs
+++ src/tests/HuggingFace.IntegrationTests/Tests.cs
@@ -39,6 +39,39 @@ public class GeneralTests
     }
     
     [TestMethod]
+    public async Task Llama2()
+    {
+        var apiKey =
+            Environment.GetEnvironmentVariable("HUGGINGFACE_API_KEY") ??
+            throw new AssertInconclusiveException("HUGGINGFACE_API_KEY environment variable is not found.");
+
+        using var client = new HttpClient();
+        var api = new HuggingFaceApi(apiKey, client);
+        var response = await api.GenerateTextAsync(
+            RecommendedModelIds.Llama2.Chat._7B,
+            new GenerateTextRequest
+            {
+                Inputs = "Give random 5 words in response",
+                Parameters = new GenerateTextRequestParameters
+                {
+                    Max_new_tokens = 250,
+                    Return_full_text = false,
+                },
+                Options = new GenerateTextRequestOptions
+                {
+                    Use_cache = true,
+                    Wait_for_model = false,
+                },
+            });
+        response.Should().NotBeEmpty();
+
+        foreach (var value in response)
+        {
+            Console.WriteLine(value.Generated_text);
+        }
+    }
+    
+    [TestMethod]
     public async Task GenerateError()
     {
         var apiKey =
