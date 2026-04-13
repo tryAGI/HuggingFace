9be313b7604a5e731e4419eb1661d65073b98717
Konstantin S. <havendv@gmail.com>
2026-04-14 01:56:13 +0400

    feat: Updated OpenAPI spec (#207)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/HuggingFace/Generated/HuggingFace.Models.CreateAgenticProvisioningResourcesRequestVariant2Configuration.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.Models.CreateAgenticProvisioningResourcesRequestVariant2Configuration.g.cs
@@ -10,7 +10,7 @@ namespace HuggingFace
     {
         /// <summary>
         /// The name of the repository (defaults to a random name if not provided)<br/>
-        /// Default Value: BRsWYDAe
+        /// Default Value: tBFkqTse
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("name")]
         public string? Name { get; set; }
@@ -34,7 +34,7 @@ namespace HuggingFace
         /// </summary>
         /// <param name="name">
         /// The name of the repository (defaults to a random name if not provided)<br/>
-        /// Default Value: BRsWYDAe
+        /// Default Value: tBFkqTse
         /// </param>
         /// <param name="visibility">
         /// The visibility of the repository (defaults to private)<br/>
