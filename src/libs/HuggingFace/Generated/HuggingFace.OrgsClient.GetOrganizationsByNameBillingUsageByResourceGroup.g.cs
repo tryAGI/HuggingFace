420f0c6c9564743ee1eabc66823cd7e73caa245a
Konstantin S. <havendv@gmail.com>
2026-06-08 15:08:36 +0400

    feat: Updated OpenAPI spec (#555)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/HuggingFace/Generated/HuggingFace.OrgsClient.GetOrganizationsByNameBillingUsageByResourceGroup.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.OrgsClient.GetOrganizationsByNameBillingUsageByResourceGroup.g.cs
@@ -54,7 +54,7 @@ namespace HuggingFace
         /// Default Value: 2026-06-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-06-08T02:36:08.216Z
+        /// Default Value: 2026-06-05T13:57:09.547Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
@@ -85,7 +85,7 @@ namespace HuggingFace
         /// Default Value: 2026-06-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-06-08T02:36:08.216Z
+        /// Default Value: 2026-06-05T13:57:09.547Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
