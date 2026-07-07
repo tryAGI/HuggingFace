484945ff10e236e8cf9e82eac65454297c6ae37f
Konstantin S. <havendv@gmail.com>
2026-07-08 00:04:37 +0400

    feat: Updated OpenAPI spec (#710)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/HuggingFace/Generated/HuggingFace.IUsersClient.GetSettingsBillingUsageByInferenceSession.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.IUsersClient.GetSettingsBillingUsageByInferenceSession.g.cs
@@ -12,7 +12,7 @@ namespace HuggingFace
         /// Default Value: 2026-07-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-07-06T16:34:02.288Z
+        /// Default Value: 2026-07-06T15:04:32.981Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
@@ -30,7 +30,7 @@ namespace HuggingFace
         /// Default Value: 2026-07-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-07-06T16:34:02.288Z
+        /// Default Value: 2026-07-06T15:04:32.981Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
