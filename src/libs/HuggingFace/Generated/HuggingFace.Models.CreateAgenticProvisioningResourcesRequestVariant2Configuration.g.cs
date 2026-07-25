080e2e9e4b6b9c1e3c81a3ea2df5c7a1327754cf
Konstantin S. <havendv@gmail.com>
2026-07-25 07:18:27 +0400

    feat: Updated OpenAPI spec (#829)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/HuggingFace/Generated/HuggingFace.Models.CreateAgenticProvisioningResourcesRequestVariant2Configuration.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.Models.CreateAgenticProvisioningResourcesRequestVariant2Configuration.g.cs
@@ -10,7 +10,7 @@ namespace HuggingFace
     {
         /// <summary>
         /// The name of the repository (defaults to a random name if not provided)<br/>
-        /// Default Value: gVpLgUon
+        /// Default Value: vIUKsjzv
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("name")]
         public string? Name { get; set; }
@@ -34,7 +34,7 @@ namespace HuggingFace
         /// </summary>
         /// <param name="name">
         /// The name of the repository (defaults to a random name if not provided)<br/>
-        /// Default Value: gVpLgUon
+        /// Default Value: vIUKsjzv
         /// </param>
         /// <param name="visibility">
         /// The visibility of the repository (defaults to private)<br/>
--- src/libs/HuggingFace/Generated/HuggingFace.OrgsClient.GetOrganizationsByNameBillingUsageByInferenceSession.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.OrgsClient.GetOrganizationsByNameBillingUsageByInferenceSession.g.cs
@@ -54,7 +54,7 @@ namespace HuggingFace
         /// Default Value: 2026-07-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-07-24T17:28:40.313Z
+        /// Default Value: 2026-07-24T15:09:44.495Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
@@ -85,7 +85,7 @@ namespace HuggingFace
         /// Default Value: 2026-07-01T00:00:00.000Z
         /// </param>
         /// <param name="endDate">
-        /// Default Value: 2026-07-24T17:28:40.313Z
+        /// Default Value: 2026-07-24T15:09:44.495Z
         /// </param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
