e611544f27a7cbe4f19ae80f1bd6247c6584aa22
Konstantin S. <havendv@gmail.com>
2026-08-14 19:58:00 +0400

    feat: Updated OpenAPI spec (#978)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/HuggingFace/Generated/HuggingFace.NotificationsClient.g.cs
+++ src/libs/HuggingFace/Generated/HuggingFace.NotificationsClient.g.cs
@@ -34,10 +34,17 @@ namespace HuggingFace
 
         /// <inheritdoc/>
         public global::HuggingFace.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::HuggingFace.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::HuggingFace.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
